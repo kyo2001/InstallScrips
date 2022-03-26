27d0ea924244a5d54b7eac2144740cca184e3cf9
kyo2001 <43395434+kyo2001@users.noreply.github.com>
2022-03-26 16:33:02 +0900

    Update servo_io_setup.sh

--- servo_io_setup.sh
+++ servo_io_setup.sh
@@ -2,7 +2,7 @@
 
 sudo raspi-config nonint do_serial 2
 
-sudo sh -c "echo 'dtoverlay=miniuart-bt\ncore_freq=250' >> /boot/config.txt"
+sudo sh -c "echo \"dtoverlay=miniuart-bt\ncore_freq=250\" >> /boot/config.txt"
 
 pip3 install rpi.gpio
 
@@ -11,4 +11,4 @@ git clone https://github.com/ROBOTIS-GIT/DynamixelSDK
 cd DynamixelSDK/python
 sudo python3 setup.py install
 
-sudo reboot
+#sudo reboot
